b51f3ff7a79108e5776d3c9a583d663b22472e0e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-05-31 12:06:46 -0700

    add sentry

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,15 @@
 import discord
+import sentry_sdk
 from discord.ext import commands
 
 from . import config
 from .bot import bot
+
+if config.sentry_dsn is not None:
+    sentry_sdk.init(
+        dsn=config.sentry_dsn,
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
+    )
 
 bot.run(config.bot_token)
