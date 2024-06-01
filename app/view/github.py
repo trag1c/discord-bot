87954f9db3a96c17b8860b8e08bcaa6626fc4f72
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-05-31 21:45:04 -0700

    app: use the org invite bot now

--- app/view/github.py
+++ app/view/github.py
@@ -11,8 +11,7 @@ class TesterWelcome(discord.ui.View):
 
     @discord.ui.button(label='Accept and Link GitHub')
     async def link(self, interaction: discord.Interaction, button: discord.ui.Button):
-        #await interaction.response.send_modal(TesterLink())
-        await interaction.response.send_modal(TesterLinkCollab())
+        await interaction.response.send_modal(TesterLink())
 
 
 class TesterLink(discord.ui.Modal, title='Link GitHub'):
@@ -49,51 +48,6 @@ class TesterLink(discord.ui.Modal, title='Link GitHub'):
             org = g.get_organization(config.github_org)
             team = org.get_team_by_slug(config.github_tester_team)
             org.invite_user(user=user, role='direct_member', teams=[team])
-
-        # Add the github role. We do this even if the user was already
-        # previously a member of the org so that they don't link another
-        # account.
-        await interaction.user.add_roles(
-            discord.Object(config.github_role_id),
-            reason="tester linked GitHub account",
-        )
-
-        await interaction.followup.send(tester_link_message, ephemeral=True)
-
-
-    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
-        await interaction.followup.send('Oops! Something went wrong.', ephemeral=True)
-        traceback.print_exception(type(error), error, error.__traceback__)
-
-
-class TesterLinkCollab(discord.ui.Modal, title='Link GitHub'):
-    """The modal shown to link a GitHub account."""
-
-    username = discord.ui.TextInput(
-        label='GitHub Username',
-        placeholder='mitchellh',
-        required=True,
-    )
-
-    async def on_submit(self, interaction: discord.Interaction):
-        # Defer since we're going to do a bunch of slow stuff.
-        await interaction.response.defer(ephemeral=True, thinking=True)
-
-        # If the user already has the github role it means they already linked.
-        if interaction.user.get_role(config.github_role_id) is not None:
-            await interaction.followup.send(tester_link_already, ephemeral=True)
-            return
-
-        # Get and verify the GitHub user
-        try:
-            user = g.get_user(self.username.value)
-        except github.UnknownObjectException:
-            await interaction.followup.send(f"GitHub user '{self.username.value}' not found.", ephemeral=True)
-            return
-
-        # If the user is already a member of the org, they're already linked.
-        repo = g.get_repo("ghostty-org/ghostty")
-        repo.add_to_collaborators(user, permission='pull')
 
         # Add the github role. We do this even if the user was already
         # previously a member of the org so that they don't link another
