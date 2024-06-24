8afb15beca1c199491da133d837b54c5c6bfdc65
trag1c <trag1cdev@yahoo.com>
2024-06-24 17:54:56 +0200

    Improved issue regex to require word boundary

--- app/issues.py
+++ app/issues.py
@@ -7,7 +7,7 @@ from discord import Message
 from app import config
 from app.github import g
 
-ISSUE_REGEX = re.compile(r"#(\d+)")
+ISSUE_REGEX = re.compile(r"#(\d+)\b")
 ISSUE_TEMPLATE = "**{kind} #{issue.number}:** {issue.title}\n{issue.html_url}\n"
 
 
