b5618938fefcf85a02e4df188f8752d5977fe772
Aarnav Tale <aarnavtale@icloud.com>
2024-09-10 14:29:00 -0400

    chore: extract message moving to utils

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+import io
+
 import discord
 from typing_extensions import TypeIs
 
 from app.setup import config
+
+MAX_ATTACHMENT_SIZE = 67_108_864  # 64 MiB
 
 
 async def server_only_warning(interaction: discord.Interaction) -> None:
@@ -25,6 +29,48 @@ async def get_or_create_webhook(
                 return webhook
 
     return await channel.create_webhook(name=name)
+
+
+async def move_message_via_webhook(
+    webhook: discord.Webhook,
+    message: discord.Message,
+    executor: discord.Member | None = None,
+) -> None:
+    content = message.content
+    uploads = []
+    skipped = 0
+
+    if message.attachments and len(message.attachments) > 0:
+        # We need to store the attachments in a buffer for a reupload
+        for attachment in message.attachments:
+            if attachment.size > MAX_ATTACHMENT_SIZE:
+                skipped += 1
+                continue
+
+            fp = io.BytesIO(await attachment.read())
+            uploads.append(discord.File(fp, filename=attachment.filename))
+
+    if executor:
+        content += f"\n-# Moved from {message.channel.mention}"
+        content += f" by {executor.mention}"
+
+    if skipped > 0:
+        # Need to add this if executor is None
+        # otherwise there will be no tiny footer
+        if executor is None:
+            content += "\n-#"
+
+        content += f" (skipped {skipped} large attachment(s))"
+
+    await webhook.send(
+        content=content,
+        username=message.author.display_name,
+        avatar_url=message.author.avatar.url,
+        allowed_mentions=discord.AllowedMentions.none(),
+        files=uploads,
+    )
+
+    await message.delete()
 
 
 def is_dm(user: discord.User | discord.Member) -> TypeIs[discord.User]:
--- app/view/mod.py
+++ app/view/mod.py
@@ -1,10 +1,8 @@
 import discord
-import io
 
 from app.setup import bot
-from app.utils import get_or_create_webhook
+from app.utils import get_or_create_webhook, move_message_via_webhook
 
-MAX_ATTACHMENT_SIZE = 67_108_864  # 64 MiB
 
 class SelectChannel(discord.ui.View):
     def __init__(self, message: discord.Message, executor: discord.Member):
@@ -30,36 +28,7 @@ class SelectChannel(discord.ui.View):
             return
 
         webhook = await get_or_create_webhook("Ghostty Moderator", channel)
-        content = self.message.content
-
-        uploads = []
-        skipped = 0
-        if self.message.attachments and len(self.message.attachments) > 0:
-            # We need to store the attachments in a buffer for a reupload
-            for attachment in self.message.attachments:
-                if attachment.size > MAX_ATTACHMENT_SIZE:
-                    skipped += 1
-                    continue
-
-                fp = io.BytesIO(await attachment.read())
-                uploads.append(discord.File(fp, filename=attachment.filename))
-
-        content += f"\n-# Moved from {self.message.channel.mention}"
-        content += f" by {self.executor.mention}"
-
-        if skipped > 0:
-            content += f" (skipped {skipped} large attachments)"
-
-        await webhook.send(
-            content=content,
-            username=self.message.author.display_name,
-            avatar_url=self.message.author.avatar.url,
-            allowed_mentions=discord.AllowedMentions.none(),
-            files=uploads,
-        )
-
-        await self.message.delete()
+        await move_message_via_webhook(webhook, self.message, self.executor)
         await interaction.response.edit_message(
-            content=f"Moved the message to {channel.mention}.",
-            view=None
+            content=f"Moved the message to {channel.mention}.", view=None
         )
