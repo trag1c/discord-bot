2a9e48e588abb550b40e4080b04d3dda0121e1bb
trag1c <trag1cdev@yahoo.com>
2024-10-31 18:30:43 +0100

    refactor: rename `id_` to `entity_id`

--- app/features/entity_mentions.py
+++ app/features/entity_mentions.py
@@ -45,19 +45,19 @@ async def handle_entities(message: Message) -> None:
 
     entities: list[str] = []
     for match in ENTITY_REGEX.finditer(message.content):
-        id_ = int(match[2])
+        entity_id = int(match[2])
         try:
-            entity = repo.get_issue(id_)
+            entity = repo.get_issue(entity_id)
             kind = "Pull Request" if entity.pull_request else "Issue"
         except github.UnknownObjectException:
             try:
-                entity = get_discussion(repo, id_)
+                entity = get_discussion(repo, entity_id)
                 kind = "Discussion"
             except github.GithubException:
                 continue
         entity_info = ENTITY_TEMPLATE.format(kind=kind, entity=entity)
         if match[1] == "#":  # Entity is referenced by number
-            if id_ < 10:  # Ignore single-digit mentions (likely a false positive)
+            if entity_id < 10:  # Ignore single-digit mentions (likely a false positive)
                 continue
             entity_info += f"{entity.html_url}\n"
         entities.append(entity_info)
